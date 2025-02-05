98fffbf553e48b93f5b8ff35e0fd1fb3aebe9f8c
Devgraices <graceadegunle16@gmail.com>
2025-02-05 16:39:11 +0100

    first commit

--- utils/mathUtils.js
+++ utils/mathUtils.js
@@ -1,23 +1,25 @@
 const isPrime = (num) => {
   if (num < 2) return false;
-  for (let i = 2; i * i <= num; i++) {
+  for (let i = 2; i * i <= Math.abs(num); i++) {
     if (num % i === 0) return false;
   }
   return true;
 };
 const isPerfect = (num) => {
   let sum = 1;
-  for (let i = 2; i * i <= num; i++) {
+  for (let i = 2; i * i <= Math.abs(num); i++) {
     if (num % i === 0) sum += i + (i !== num / i ? num / i : 0);
   }
-  return sum === num && num !== 1;
+  return sum === Math.abs(num) && num !== 1;
 };
 const isArmStrong = (num) => {
-  const digits = num.toString().split("").map(Number);
-  return digits.reduce((sum, d) => sum + d ** digits.length, 0) === num;
+  const digits = Math.abs(num).toString().split("").map(Number);
+  return (
+    digits.reduce((sum, d) => sum + d ** digits.length, 0) === Math.abs(num)
+  );
 };
 const getDigitSum = (num) => {
-  return num
+  return Math.abs(num)
     .toString()
     .split("")
     .reduce((sum, d) => sum + parseInt(d), 0);
